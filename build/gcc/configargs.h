2c68350766d7f7a849bddb558c8ad980d6c15069
Francis Ssessaazi <phrunsys.scpel.256.ug@gmail.com>
2025-01-22 18:14:17 +0300

    Delete build directory - It was a mistake leaving it here

--- build/gcc/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --enable-languages=c,c++ --disable-multilib --disable-bootstrap --enable-shared --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-libunwind-exceptions --enable-gnu-unique-object --with-gcc-major-version-only --enable-linker-build-id --with-linker-hash-style=gnu --enable-plugin --enable-initfini-array --enable-gnu-indirect-function --with-tune=generic --build=x86_64-linux-gnu";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "generic" } };
--- build/gcc/gcc-driver-name.h
+++
@@ -1,1 +0,0 @@
-#define GCC_DRIVER_NAME "x86_64-linux-gnu-gcc-15"
